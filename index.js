a4b37eec5d7ce27562719fcda5d4020085100089
damin <34189426+damil27@users.noreply.github.com>
2023-04-20 13:21:25 +0100

    environment variable changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 const cors = require("cors");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -17,15 +17,15 @@ app.use(cors());
 const pool = mysql.createPool({
   connectionLimit: 10,
 
-  host: "208.91.199.159",
-  user: "providen_Ik",
-  password: "u7brAkxs%&,g",
-  database: "providen_esp_data_acquisition",
+  // host: "208.91.199.159",
+  // user: "providen_Ik",
+  // password: "u7brAkxs%&,g",
+  // database: "providen_esp_data_acquisition",
 
-  // host: "localhost",
-  // user: "root",
-  // password: "",
-  // database: "esp_data_acquisition",
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "esp_data_acquisition",
 });
 // get request from database
 app.get("/", (req, res) => {
