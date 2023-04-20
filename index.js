43e7c6b81cf318ae34d3fcd1e3045ba0c0bdd0f6
damin <34189426+damil27@users.noreply.github.com>
2023-04-20 13:22:10 +0100

    connection paramter adjusted

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ app.use(cors());
 const pool = mysql.createPool({
   connectionLimit: 10,
 
-  // host: "208.91.199.159",
-  // user: "providen_Ik",
-  // password: "u7brAkxs%&,g",
-  // database: "providen_esp_data_acquisition",
+  host: "208.91.199.159",
+  user: "providen_Ik",
+  password: "u7brAkxs%&,g",
+  database: "providen_esp_data_acquisition",
 
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "esp_data_acquisition",
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "esp_data_acquisition",
 });
 // get request from database
 app.get("/", (req, res) => {
