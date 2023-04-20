d151c669547f3df55e7fb2d03acc73336313aba2
damin <34189426+damil27@users.noreply.github.com>
2023-04-20 13:03:08 +0100

    ESP32 BACKEND

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+// mysql  connection
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+
+  host: "208.91.199.159",
+  user: "providen_Ik",
+  password: "u7brAkxs%&,g",
+  database: "providen_esp_data_acquisition",
+
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "esp_data_acquisition",
+});
+// get request from database
+app.get("/", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    connection.query(" SELECT * FROM data_linking", (err, rows) => {
+      connection.release();
+      if (!err) {
+        res.send(rows);
+      } else {
+        console.log(err);
+      }
+    });
+  });
+});
+app.listen(port, () => console.log(`app listening on port ${port}`));
